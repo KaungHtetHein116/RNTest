51e40c15880d051384463122bd7769a3a4f87420
kaunghtethein977 <kaungkaung116.kk@gmail.com>
2021-01-16 15:26:32 +0630

    picker

--- src/screens/HistoryScreen.js
+++ src/screens/HistoryScreen.js
@@ -1,31 +1,94 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, Alert} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Picker, StyleSheet, FlatList} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {openDatabase} from 'react-native-sqlite-storage';
 
-var db = openDatabase({name: 'ItemDatabase.db'});
+const db = openDatabase({name: 'ItemDatabase.db'});
+const YEAR = [2020, 2021, 2022, 2023, 2024];
+const MONTH = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export default function HistoryScreen() {
+  const dateObj = new Date();
+
+  const [data, setData] = useState([]);
+  const [month, setMonth] = useState(dateObj.getUTCMonth() + 1);
+  const [year, setYear] = useState(dateObj.getUTCFullYear());
+
   const handleSelectYearMonthData = () => {
     db.transaction((tx) => {
       tx.executeSql(
-        'SELECT * FROM item_table where year=? && month=?',
-        [2021, 2],
+        'SELECT * FROM item_table where year=? and month=?',
+        [year, month],
         (tx, results) => {
           var temp = [];
           for (let i = 0; i < results.rows.length; ++i)
             temp.push(results.rows.item(i));
           console.log('history', temp);
+          setData(temp);
         },
       );
     });
   };
+
+  useEffect(() => {
+    handleSelectYearMonthData();
+    console.log(month, year);
+  }, []);
+
+  const pickerItem = () => {
+    return MONTH.map((month) => {
+      return <Picker.Item label={month} value={month} key={month} />;
+    });
+  };
+
   return (
-    <View style={{flexDirection: 'row'}}>
-      {/* <Text>{item.item_price}</Text> */}
-      <TouchableOpacity onPress={() => handleSelectYearMonthData()}>
-        <AntDesign name="delete" size={30} />
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={{flexDirection: 'row'}}>
+        <Text>Pick month</Text>
+        <Picker
+          selectedValue={month}
+          style={{height: 50, width: 100}}
+          onValueChange={(itemValue) => setMonth(itemValue)}>
+          {pickerItem()}
+        </Picker>
+        <Text>Pick year</Text>
+        <Picker
+          selectedValue={year}
+          style={{height: 50, width: 100}}
+          onValueChange={(itemValue) => setYear(itemValue)}>
+          <Picker.Item label="2020" value="2020" />
+          <Picker.Item label="2021" value="2021" />
+        </Picker>
+      </View>
+      <View style={{}}>
+        <FlatList
+          data={data}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({item}) => (
+            <View style={styles.itemContainer}>
+              <Text>Date Created: {item.date}</Text>
+              <Text>Item Name : {item.item_name}</Text>
+              <Text>Item Price: {item.item_price}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    margin: 10,
+    padding: 20,
+    justifyContent: 'space-between',
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    elevation: 10,
+    borderRadius: 10,
+  },
+  container: {
+    flex: 1,
+  },
+});
--- src/screens/components/ItemComponent.js
+++ src/screens/components/ItemComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, Alert} from 'react-native';
+import {View, Text, TouchableOpacity, Alert, StyleSheet} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {openDatabase} from 'react-native-sqlite-storage';
 
@@ -34,10 +34,23 @@ export default function ItemComponent({item, getItem}) {
   };
   return (
     <View style={{flexDirection: 'row'}}>
-      <Text>{item.item_price}</Text>
-      <TouchableOpacity onPress={() => handlePressDelete(item.item_id)}>
-        <AntDesign name="delete" size={30} />
-      </TouchableOpacity>
+      <View style={styles.itemContainer}>
+        <Text>{item.item_name}</Text>
+        <Text>{item.item_price}</Text>
+        <Text>{item.date}</Text>
+        <TouchableOpacity onPress={() => handlePressDelete(item.item_id)}>
+          <AntDesign name="delete" size={30} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    margin: 10,
+    padding: 20,
+    justifyContent: 'space-between',
+  },
+});
